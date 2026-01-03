441fb9ab4b03fb35ad9f8ae39be4c5c046b1b2b9
MUHAMMAD MAHTAB FAISAL <cursedloop325@gmail.com>
2026-01-03 14:45:38 +0500

    Add functionality to update movie seats

--- movie manager.py
+++ movie manager.py
@@ -44,4 +44,28 @@ def remove_movie():
     except:
         print("❌ Invalid input!")
 
+def update_seats():
+    movies = view_movie()
+    if not movies:
+        return
 
+    try:
+        choice = int(input("Select movie number: "))
+        change = int(input("Seats -(number) or +(number): "))
+
+        data = movies[choice - 1].strip().split(",")
+        seats = int(data[4]) + change
+
+        if seats < 0:
+            print("❌ Seats cannot be negative!")
+            return
+
+        data[4] = str(seats)
+        movies[choice - 1] = ",".join(data) + "\n"
+
+        with open("MOVIE_FILE.txt", "w") as f:
+            f.writelines(movies)
+
+        print("✅ Seats updated!")
+    except:
+        print("❌ Invalid input!")
