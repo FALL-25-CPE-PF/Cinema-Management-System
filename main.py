f50b068991c67012316e5cc348cb1a8b93098fac
MUHAMMAD MAHTAB FAISAL <cursedloop325@gmail.com>
2026-01-03 15:11:25 +0500

    Add main menu for cinema management system

---
+++ main.py
@@ -0,0 +1,19 @@
+from admin import admin_mode
+
+while True:
+    print("\n===== CINEMA MANAGEMENT SYSTEM =====")
+    print("1. Admin Mode")
+    print("2. User Mode")
+    print("3. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        admin_mode()
+    # elif choice == "2":
+    #     user_mode()
+    elif choice == "3":
+        print("\nThank you for using the system!")
+        break
+    else:
+        print(" \nInvalid choice !!!")
