97870f95ce1a1f81e459cd6e613dd0a2a7202c0b
MUHAMMAD MAHTAB FAISAL <cursedloop325@gmail.com>
2026-01-03 13:59:22 +0500

    Add movie viewing functionality

--- movie manager.py
+++ movie manager.py
@@ -1,3 +1,4 @@
+#------------ADD MOVIES------------------#
 def add_movie():
     name = input("Movie name: ")
     category = input("Category: ")
@@ -9,3 +10,19 @@ def add_movie():
         f.write(f"{name},{category},{time},{price},{seats}\n")
 
     print("✅ Movie added successfully!")
+
+#----------VIEW MOVIES-----------------#
+def view_movie():
+    try:
+        with open("MOVIE_FILE.txt","r") as f:
+            movies = f.readlines()
+        if not movies:
+            print("Currently No Movies Available")
+            return movies
+        print("\n--- AVAILABLE MOVIES ---")
+        for i, m in enumerate(movies, start=1):
+            name, category, time, price, seats = m.strip().split(",")
+            print(f"{i}. {name} | {category} | {time} | Rs {price} | Seats: {seats}")
+        return movies
+    except FileNotFoundError:
+        print("System Hacked all data Deleted!!!")
